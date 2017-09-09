3d393bf2a803755da9d152092e21e12fc3ecdd73
liaozeen <liaogogogo@qq.com>
2017-09-09 16:08:55 +0800

    设置数据获取接口

--- build/dev-server.js
+++ build/dev-server.js
@@ -21,6 +21,39 @@ var autoOpenBrowser = !!config.dev.autoOpenBrowser
 var proxyTable = config.dev.proxyTable
 
 var app = express()
+
+//用于数据读取
+var appData = require('../data.json');
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
+
+//路由
+var apiRoutes = express.Router();
+
+apiRoutes.get('/seller',function (req,res) {
+    res.json({
+      erron:0,
+      data:seller
+    });
+})
+
+apiRoutes.get('/goods',function (req,res) {
+  res.json({
+    erron:0,
+    data:goods
+  });
+})
+
+apiRoutes.get('/ratings',function (req,res) {
+  res.json({
+    erron:0,
+    data:ratings
+  });
+})
+
+app.use('/api',apiRoutes);
+
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
