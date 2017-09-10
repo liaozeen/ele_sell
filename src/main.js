e5a0b38c615f9503bd2b46ee86eec9a84643d93d
liaozeen <liaogogogo@qq.com>
2017-09-10 11:41:06 +0800

    添加vue-router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
+import goods from './components/goods/goods.vue';
+import ratings from './components/ratings/ratings.vue';
+import seller from './components/seller/seller.vue';
 
-Vue.config.productionTip = false;
+/* 安装vue-router */
+Vue.use(VueRouter);
 
-/* eslint-disable no-new */
+let router = new VueRouter({
+  routes: [
+    { path: '/goods', component: goods },
+    { path: '/ratings', component: ratings },
+    { path: '/seller', component: seller }
+  ]
+});
+
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
-});
+  router,
+  render: h => h(App)
+}).$mount('#app');
+
+router.go('/goods');
