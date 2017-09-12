28800c4dc3b98d3f6675d692c427661c93419749
liaozeen <liaogogogo@qq.com>
2017-09-12 23:26:02 +0800

    完成头部和弹层页的样式和功能

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,24 +29,40 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
-    <div v-show="detailShow"  class="detail">
-      <div class="detail-wrapper clearfix">
-        <div class="detail-main">
-          <div class="name">{{seller.name}}</div>
-          <div class="star-wraper">
-            <star :size="48" :score="seller.score"></star>
-          </div>
-          <div class="title">
-            <div class="line"></div>
-            <div class="text">优惠信息</div>
-            <div class="line"></div>
+    <transition name="fade">
+      <div v-show="detailShow"  class="detail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <div class="name">{{seller.name}}</div>
+            <div class="star-wraper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item , index) in seller.supports">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
           </div>
         </div>
+        <div class="detail-close">
+          <i class="icon-close" @click="closeDetail"></i>
+        </div>
       </div>
-      <div class="detail-close">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -61,12 +77,15 @@
     },
     data () {
       return {
-        detailShow: true
+        detailShow: false
       };
     },
     methods: {
       showDetail () {
         this.detailShow = true;
+      },
+      closeDetail () {
+        this.detailShow = false;
       }
     },
     created () {
@@ -80,6 +99,7 @@
 
 <style lang="stylus" type="stylesheet/stylus">
   @import "../../common/stylus/mixin.styl";
+
     .header
       position: relative
       overflow: hidden
@@ -198,7 +218,14 @@
         width:100%
         height:100%
         overflow:auto
-        background:rgba(7,17,27,0.8)
+        backdrop-filter: blur(10px)
+        opacity:1
+        background: rgba(7,17,27,0.8)
+        &.fade-enter-active, &.fade-leave-active
+          transition:all 0.5s
+        &.fade-enter, &.fade-leave-active
+          opacity: 0
+          background:rgba(7,17,27,0)
         .detail-wrapper
           min-height: 100%
           width:100%
@@ -217,7 +244,7 @@
             .title
               display: flex
               width:80%
-              margin:30px auto 24px auto
+              margin:28px auto 24px auto
               .line
                 flex:1
                 position: relative
@@ -225,7 +252,46 @@
                 border-bottom: 1px solid rgba(255,255,255,0.2)
               .text
                 padding:0 12px
+                font-weight:700
                 font-size:14px
+            .supports
+              width:80%
+              margin:0 auto
+              .support-item
+                 padding: 0 12px
+                 margin-bottom:12px
+                 font-size:0
+                 &.last-child
+                   margin-bottom:0
+                .icon
+                  display:inline-block
+                  vertical-align: top
+                  width:16px
+                  height: 16px
+                  margin-right:6px
+                  background-size: 16px 16px
+                  background-repeat: no-repeat
+                  &.decrease
+                   bg-image('decrease_2')
+                  &.discount
+                   bg-image('discount_2')
+                  &.guarantee
+                   bg-image('guarantee_2')
+                  &.invoice
+                    bg-image('invoice_2')
+                  &.special
+                    bg-image('special_2')
+                .text
+                  line-height:16px
+                  font-size:12px
+            .bulletin
+               width:80%
+               margin:0 auto
+               .content
+                padding:0 12px
+                font-size:12px
+                font-weight:200
+                line-height:24px
         .detail-close
           position: relative
           width: 32px
