1dfd20fa89b0a19a0055cb9c146b3686cfdc40a6
liaozeen <liaogogogo@qq.com>
2017-09-13 21:32:24 +0800

    商品页两栏flex布局-左边宽度固定，右边宽度自适应

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>I am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -7,5 +10,16 @@
 </script>
 
 <style lang="stylus" type="stylesheet/stylus">
-
+  .goods
+    display:flex
+    position: absolute
+    top:174px
+    bottom:46px
+    width:100%
+    overflow:hidden
+    .menu-wrapper
+      flex:0 0 80px
+      background:#f3f5f7
+    .foods-wrapper
+      flex:1
 </style>
