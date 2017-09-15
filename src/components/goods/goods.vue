14e864b964cc4cbdbec61f939f1335ba58889ca8
liaozeen <liaogogogo@qq.com>
2017-09-15 16:20:11 +0800

    实现列表滚动

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
         <li v-for="item in goods" class="menu-item">
           <span class="text border-1px">
@@ -10,7 +10,7 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
         <li v-for="item in goods" class="food-list">
           <h1 class="title">{{item.name}}</h1>
@@ -40,6 +40,8 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+
   const ERR_OK = 0;
 
   export default {
@@ -59,8 +61,18 @@
         response = response.body;
         if (response.erron === ERR_OK) {
           this.goods = response.data;
+            this.$nextTick(() => {
+              this._initScroll();
+            });
         }
       });
+    },
+    methods: {
+      _initScroll () {
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {});
+
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {});
+      }
     }
   };
 </script>
