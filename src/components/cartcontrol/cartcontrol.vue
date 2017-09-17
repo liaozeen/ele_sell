d26a4f789463400553b07471658feb3e2d09fa1d
liaozeen <liaogogogo@qq.com>
2017-09-17 16:47:11 +0800

    购物车按钮水平滚动动画效果

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click="decreaseCart"></div>
+    <transition name="move">
+      <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">
+        <span class="inner icon-remove_circle_outline"></span>
+      </div>
+    </transition>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add icon-add_circle" @click="addCart"></div>
   </div>
@@ -41,12 +45,25 @@
 <style lang="stylus" type="stylesheet/stylus">
   .cartcontrol
     font-size: 0
-    .cart-decrease,.cart-add
+    .cart-decrease
       display:inline-block
       padding:6px
-      font-size:24px
-      line-height:24px
-      color:rgb(0,160,220)
+      opacity: 1
+      transform: translate3d(0, 0, 0)
+      .inner
+        display:inline-block
+        font-size:24px
+        line-height:24px
+        color:rgb(0,160,220)
+        transition:all 0.4s linear
+        transform:rotate(0)
+      &.move-enter-active, &.move-leave-active
+        transition: all 0.5s linear
+      &.move-enter, &.move-leave-active
+        opacity: 0
+        transform: translate3d(24px, 0, 0)
+        .inner
+          transform: rotate(180deg)
     .cart-count
       display:inline-block
       vertical-align:top
@@ -58,4 +75,8 @@
       color:rgb(147,153,159)
     .cart-add
       display:inline-block
+      padding:6px
+      font-size:24px
+      line-height:24px
+      color:rgb(0,160,220)
 </style>
