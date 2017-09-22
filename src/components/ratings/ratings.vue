681bde2aa00f7a4bf35df9080ea5a97cdcaf6b6a
liaozeen <liaogogogo@qq.com>
2017-09-22 15:59:33 +0800

    评论页-综述部分右栏内容布局和样式基本实现

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -7,18 +7,38 @@
           <div class="title">综合评分</div>
           <div class="rank">高于周边商家{{seller.rankRate}}%</div>
         </div>
-        <div class="overview-right">you</div>
+        <div class="overview-right">
+          <div class="score-wraper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wraper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import star from '../star/star';
+
   export default {
     props: {
       seller: {
         type: Object
       }
+    },
+    components: {
+      star
     }
   };
 </script>
@@ -56,5 +76,35 @@
           color:rgb(147,153,159)
       .overview-right
         flex:1
-        padding-left:24px
+        padding:6px 0 6px 24px
+        .score-wraper
+          margin-bottom:8px
+          font-size:0
+          .title
+            display:inline-block
+            line-height:18px
+            vertical-align:top
+            font-size:12px
+            color:rgb(7,17,27)
+          .star
+            display:inline-block
+            margin:0 12px
+            vertical-align:top
+          .score
+            display:inline-block
+            vertical-align:top
+            font-size:12px
+            line-height:18px
+            color:rgb(255,153,0)
+        .delivery-wrapper
+          font-size:0
+          .title
+            display:inline-block
+            line-height:18px
+            font-size:12px
+            color:rgb(7,17,27)
+          .delivery
+            margin-left:12px
+            font-size:12px
+            color:rgb(147,153,159)
 </style>
