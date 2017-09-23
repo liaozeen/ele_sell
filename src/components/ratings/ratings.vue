1d568597cf2db0adc86773383dbd2f4b6dd5e260
liaozeen <liaogogogo@qq.com>
2017-09-23 09:41:47 +0800

    评价选项与评价列表内容关联

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -25,10 +25,10 @@
         </div>
       </div>
       <split></split>
-      <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="ratings" ></ratingselect>
+      <ratingselect :select-type="selectType" :only-content="onlyContent"  :ratings="ratings" v-on:ratingtypeSelect="_ratingtypeSelect" v-on:contentToggle="_contentToggle"></ratingselect>
       <div class="rating-wrapper">
         <ul>
-          <li v-for="rating in ratings" class="raing-item">
+          <li v-for="rating in ratings"  v-show="needShow(rating.rateType, rating.text)" class="rating-item">
             <div class="avatar">
               <img width="28" height="28" :src="rating.avatar">
             </div>
@@ -36,12 +36,12 @@
               <h1 class="name">{{rating.username}}</h1>
               <div class="star-wrapper">
                 <star :size="24" :score="rating.score"></star>
-                <span class="delivery" v-show="rating.delivery">{{rating.deliveryTme}}</span>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
               </div>
               <p class="text">{{rating.text}}</p>
               <div class="recommend" v-show="rating.recommend  && rating.recommend.length">
                 <span class="icon-thumb_up"></span>
-                <span v-for="item in rating.recommend">{{item}}</span>
+                <span class="item" v-for="item in rating.recommend">{{item}}</span>
               </div>
               <div class="time">{{rating.rateTime | formatDate}}</div>
             </div>
@@ -88,6 +88,31 @@
         }
       });
     },
+    methods: {
+      needShow (type, text) {
+        if (this.onlyContent && !text) {
+          return false;
+        }
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return type === this.selectType;
+        }
+      },
+      _ratingtypeSelect (type) {
+        this.selectType = type;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      _contentToggle (onlyContent) {
+        this.onlyContent = !this.onlyContent;
+        // this.onlyContent = onlyContent;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      }
+    },
     filters: {
       formatDate (time) {
         let date = new Date(time);
@@ -103,6 +128,8 @@
 </script>
 
 <style lang="stylus" type="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl";
+
   .ratings
     position:absolute
     top:174px
@@ -169,5 +196,65 @@
           .delivery
             margin-left:12px
             font-size:12px
+            color:rgb(147,153,159)
+    .rating-wrapper
+      padding:0 18px
+      .rating-item
+        display:flex
+        padding:18px 0
+        border-1px(rgba(7,17,27,0.1))
+        .avatar
+          flex:0 0 28px
+          width:28px
+          margin-right:12px
+          img
+            border-radius:50%
+        .content
+          position:relative
+          flex:1
+          .name
+            margin-bottom:4px
+            line-height:12px
+            font-size:10px
+            color:rgb(7,17,27)
+          .star-wrapper
+            margin-bottom:6px
+            font-size:0
+            .star
+              display:inline-block
+              margin-right:6px
+              vertical-align:top
+            .delivery
+              display:inline-block
+              vertical-align:top
+              line-height:12px
+              font-size:10px
+              color:rgb(147,153,159)
+          .text
+            margin-bottom: 8px
+            line-height: 18px
+            color:rgb(7,17,27)
+            font-size: 12px
+          .recommend
+            line-height:16px
+            font-size: 0
+            .icon-thumb_up, .item
+              display:inline-block
+              margin:0 8px 4px 0
+              font-size:9px
+            .icon-thumb_up
+              color:rgb(0,160,220)
+            .item
+              padding:0 6px
+              border:1px solid rgba(7,17,27,0.1)
+              border-redius:1px
+              color:rgb(147,153,159)
+              background:#fff
+          .time
+            position:absolute
+            top:0
+            right:0
+            line-height:12px
+            font-size:10px
             color:rgb(147,153,159)
 </style>
