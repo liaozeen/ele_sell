1f8eca640405910e3edc1ad34c9cc77be44e6651
liaozeen <liaogogogo@qq.com>
2017-09-23 16:16:42 +0800

    商家页：公告与活动布局和样式实现

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -30,6 +30,18 @@
         </ul>
       </div>
       <split></split>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper  border-1px">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item border-1px" v-for="(item , index) in seller.supports">
+            <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+            <span class="text">{{seller.supports[index].description}}</span>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -41,6 +53,9 @@
   export default {
     props: {
       seller: Object
+    },
+    created () {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     },
     components: {
       star,
@@ -101,4 +116,45 @@
             color:rgb(7,17,27)
             .stress
               font-size:24px
+    .bulletin
+      padding:18px 18px 0 18px
+      .title
+        font-size:14px
+        line-height:14px
+        color:rgb(7,17,27)
+        margin-bottom:8px
+      .content-wrapper
+        padding:0 12px 16px 12px
+        border-1px(rgba(7,17,27,0.1))
+        .content
+          line-height:24px
+          font-size:12px
+          color:rgb(240,20,20)
+      .supports
+        .support-item
+          padding:16px 12px
+          border-1px(rgba(7,17,27,0.1))
+          font-size:0
+          .icon
+            display:inline-block
+            vertical-align: top
+            width:16px
+            height: 16px
+            margin-right:6px
+            background-size: 16px 16px
+            background-repeat: no-repeat
+            &.decrease
+              bg-image('decrease_4')
+            &.discount
+              bg-image('discount_4')
+            &.guarantee
+              bg-image('guarantee_4')
+            &.invoice
+              bg-image('invoice_4')
+            &.special
+              bg-image('special_4')
+          .text
+            line-height:16px
+            font-size:12px
+            color:rgb(7,17,27)
 </style>
