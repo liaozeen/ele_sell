6b47462a909a7c47b4f64f299a7a89423cf8a387
liaozeen <liaogogogo@qq.com>
2017-09-23 21:48:00 +0800

    商家页-商家信息部分实现

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -18,13 +18,13 @@
           <li class="block">
             <h2>商家配送</h2>
             <div class="content">
-              <span class="stress">{{seller.minPrice}}</span>元
+              <span class="stress">{{seller.deliveryPrice}}</span>元
             </div>
           </li>
           <li class="block">
             <h2>平均配送时间</h2>
             <div class="content">
-              <span class="stress">{{seller.minPrice}}</span>分钟
+              <span class="stress">{{seller.deliveryTime}}</span>分钟
             </div>
           </li>
         </ul>
@@ -40,6 +40,24 @@
             <span class="icon" :class="classMap[seller.supports[index].type]"></span>
             <span class="text">{{seller.supports[index].description}}</span>
           </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper"  ref="picWrapper">
+          <ul class="pic-list" ref="picList">
+            <li class="pic-item" v-for="pic in seller.pics">
+              <img :src="pic" width="120" height="90">
+            </li>
+          </ul>
+        </div>
+      </div>
+      <split></split>
+      <div class="info">
+        <h1 class="title" border-1px>商家信息</h1>
+        <ul>
+          <li class="info-item" v-for="info in seller.infos">{{info}}</li>
         </ul>
       </div>
     </div>
@@ -61,12 +79,14 @@
       'seller' () {
         this.$nextTick(() => {
           this._initScroll();
+          this._initPics();
         });
       }
     },
     mounted () {
       this.$nextTick(() => {
         this._initScroll();
+        this._initPics();
       });
     },
     methods: {
@@ -77,6 +97,26 @@
           });
         } else {
           this.scroll.refresh();
+        }
+      },
+      _initPics () {
+        if (this.seller.pics) {
+          let picWidth = 120;
+          let margin = 6;
+          //  计算图片的总宽度
+          let width = (picWidth + margin) * this.seller.pics.length - margin;
+          // 设置图片的总宽度
+          this.$refs.picList.style.width = width + 'px';
+          this.$nextTick(() => {
+            if (!this.picScroll) {
+              this.picScroll = new BScroll(this.$refs.picWrapper, {
+                scrollX: true,
+                eventPassthrough: 'vertical'
+              });
+            } else {
+              this.picScroll.refresh();
+            }
+          });
         }
       }
     },
@@ -185,4 +225,39 @@
             line-height:16px
             font-size:12px
             color:rgb(7,17,27)
+    .pics
+      padding:18px
+      .title
+        font-size:14px
+        line-height:14px
+        color:rgb(7,17,27)
+        margin-bottom:12px
+      .pic-wrapper
+        width:100%
+        overflow:hidden
+        white-space:nowrap
+        .pic-list
+          font-size:0
+          .pic-item
+            display:inline-block
+            margin-right:6px
+            width:120px
+            height:90px
+            &:last-child
+              margin:0
+    .info
+      padding:18px 18px 0 18px
+      color: rgb(7, 17, 27)
+      .title
+        padding-bottom: 12px
+        line-height: 14px
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size: 14px
+      .info-item
+        padding: 16px 12px
+        line-height: 16px
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size: 12px
+        &:last-child
+          border-none()
 </style>
